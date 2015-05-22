08e368f133c6121a5137ccb27ac0eead28864d56
lag3o <lag_dab@hotmail.de>
2015-05-22 16:04:28 +0200

    protection off

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def login
-    @user = User.find_by_sql(['select * from users Where identity like ?;', params[:identity]])
+    @user = User.find_by_sql(['select salt_masterkey from users Where identity like ?;', params[:identity]])
     if (@user)
-      respond_with(@user[:salt_masterkey], @user.privkey_user_enc, @user.pubkey_user, status: 100)
+      respond_with(@user.salt_masterkey, @user.privkey_user_enc, @user.pubkey_user, status: 100)
     else
       respond_with(status: 101)
     end
