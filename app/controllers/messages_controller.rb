973734d6bebf1d53537a4b4f5312597a7fd3212d
lag3o <lag_dab@hotmail.de>
2015-05-24 12:13:12 +0200

    bug fixing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
 
   def showAll
 
-    @message = Message.find_by_sql(['select m.id, u.identity as recipient, send.identity as sender, m.recipient_id
+    messages = Message.find_by_sql(['select m.id, u.identity as recipient, send.identity as sender, m.recipient_id
                                       from messages m
                                       join users u
                                           on m.recipient_id= u.user_id
