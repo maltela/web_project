52fbec2a82714e8ba250c6d746d66e4f959a6302
lag3o <lag_dab@hotmail.de>
2015-05-24 16:49:21 +0200

    bug fixing

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -57,7 +57,7 @@ class MessagesController < ApplicationController
       @recipient = User.find_by_sql(['select * from users Where identity like ?;', params[:recipient]])
       service = {:envelope => params[:inner_envelope], :recipient => params[:recipient]}
       digest = OpenSSL::Digest.new('sha256')
-      sig_service = OpenSSL::HMAC.hexdigest(digest, @sender.first.privkey_user_enc, sig_service)
+      sig_service = OpenSSL::HMAC.hexdigest(digest, @sender.first.privkey_user_enc, service.to_string)
       #@recipient = User.find_by_identity(newMessage.identity)
      #@message = Message.new(:cipher => params[:cipher], :sig_recipient => params[:sig_recipient], :iv => params[:iv], :key_recipient_enc => params[:key_recipient_enc], :sender_id => @sender.first.user_id, :recipient_id => @recipient.first.user_id)
       if ((@sender) && (@recipient))
