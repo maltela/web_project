5c11eaf54c51413a399dbea0d84ae46d70589dd5
lag3o <lag_dab@hotmail.de>
2015-05-24 12:56:45 +0200

    bug fixing

--- db/migrate/20150424133805_create_messages.rb
+++ db/migrate/20150424133805_create_messages.rb
@@ -7,6 +7,7 @@ class CreateMessages < ActiveRecord::Migration
       t.string :sig_recipient
       t.string :iv
       t.string :key_recipient_enc
+      t.boolean :read
       t.timestamps
     end
   end
