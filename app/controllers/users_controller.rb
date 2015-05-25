7eb517ae0276fc57a9fabce77bae3b63eb1413c2
lag3o <lag_dab@hotmail.de>
2015-05-25 12:37:03 +0200

    bug fixing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def register
 
-    @privkey_user_enc = Base64.decode64(params[:privkey_user_enc])
+    @privkey_user_enc = {:privkey_user_enc => Base64.decode64(params[:privkey_user_enc])}
     render json: @privkey_user_enc
     #@user = User.new(:identity => params[:identity], :salt_masterkey => params[:salt_masterkey], :pubkey_user => params[:pubkey_user], :privkey_user_enc => @privkey_user_enc)
      # if !(User.find_by_identity(@user.identity))
