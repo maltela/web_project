51b082a12824e424d9669b36040b417f4f6cd23e
lag3o <lag_dab@hotmail.de>
2015-06-06 12:59:31 +0200

    Verify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
   def pubKey
     @user = User.find_by_identity(params[:identity])
-    if (User.find_by_identity(@user.identity))
+    if (User.find_by_identity(@user.first.identity))
       @pubkey = {:pubkey_user => @user.pubkey_user, :status_code => 112}
     else
       @pubkey = {:status_code => 411}
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def login
     @user = User.find_by_sql(['select * from users Where identity like ?;', params[:identity]])
-    if (User.find_by_identity(@user.identity))
+    if (User.find_by_identity(@user.first.identity))
       @userdata =  {:salt_masterkey => @user.first.salt_masterkey, :privkey_user_enc => @user.first.privkey_user_enc, :pubkey_user => @user.first.pubkey_user, :status => 111}
     else
       @userdata = {:status => 411}
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   def register
 
     @user = User.new(:identity => params[:identity], :salt_masterkey => params[:salt_masterkey], :pubkey_user => params[:pubkey_user], :privkey_user_enc => params[:privkey_user_enc])
-      if !(User.find_by_identity(@user.identity))
+      if !(User.find_by_identity(@user.first.identity))
           if @user.save
             @status_code = {:status_code => 110}
           else
