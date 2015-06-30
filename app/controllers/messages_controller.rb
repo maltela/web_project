0cd6424e95d8cbeb7f359787999185a04bd01ae4
lag3o <lag_dab@hotmail.de>
2015-06-30 17:06:01 +0200

    Signature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -69,9 +69,9 @@ class MessagesController < ApplicationController
       key = OpenSSL::PKey::RSA.new(Base64.decode64(@recipient.pubkey_user))
       puts @sender.first.identity
       puts @recipient.first.identity
-     if ((User.find_by_identity(@sender.first.identity)) && User.find_by_identity(@recipient.first.identity))
+     if ((@sender.identity) && (@recipient.identity))
        if (key.verify digest, Base64.decode64(params[:sig_service]), sig_service)
-        @message = Message.new(:cipher => params[:inner_envelope][:cipher], :sig_recipient => params[:inner_envelope][:sig_recipient], :iv => params[:inner_envelope][:iv], :key_recipient_enc => params[:inner_envelope][:key_recipient_enc], :sender_id => @sender.first.user_id, :recipient_id => @recipient.first.user_id, :read => false)
+        @message = Message.new(:cipher => params[:inner_envelope][:cipher], :sig_recipient => params[:inner_envelope][:sig_recipient], :iv => params[:inner_envelope][:iv], :key_recipient_enc => params[:inner_envelope][:key_recipient_enc], :sender_id => @sender.user_id, :recipient_id => @recipient.user_id, :read => false)
         if (@message)
           respond_to do |format|
             if @message.save
